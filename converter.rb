1c9c2bd89db3e94144c5bb45b9a505c9fdcf12ac
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2020-02-18 18:18:29 +0700

    initialize converter function

---
+++ converter.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+class Converter
+  def initialize(folder_destination)
+    @folder_destination = folder_destination.chomp.strip
+  end
+  
+  def call
+    #TODO: Add main function to count on how many files inside a path with the same content.
+  end
+end
